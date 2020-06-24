9f2678706458465136d82c55dd4ce3cdf7b327b5
unknown <brw1820@gmail.com>
2020-06-23 22:08:41 -0400

    revisions to weather dashboard homework

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4e9688c472c7e00d4dcc313124c3c2a7
 $(document).ready(function () {
   console.log("hello world");
-  // $('.time').text(moment().format('MMMM Do YYYY'));
+  $(".time").text(moment().format('MMMM Do YYYY'));
+
   $("#city-location").on("click", function () {
     // event.preventDefault();
+    $(".forecast-result").empty();
+    
     var cityName = $("#city-search").val();
     // Create "li" element and append it to "ul" list
     var button = $("<button>");
@@ -19,12 +22,34 @@ $(document).ready(function () {
     var forecastURL =
       "https://api.openweathermap.org/data/2.5/forecast?q=" +
       cityName +
-      "&units=imperial&appid=27b2cedd2dfb2b4e4198640c8d09052b";
+      "&units=imperial&appid=" + APIKey;
 
-    // console.log(forecastURL);
-    // var cityNameArray = [];
-    // localStorage.setItem(cityName, JSON.stringify(cityName));
-    // console.log(localStorage.setItem(cityName, JSON.stringify(cityNameArray)));
+    
+  //   function storeL()  {
+  //   var cityNameArray = [];
+  //   localStorage.setItem("cityName", JSON.stringify(cityNameArray));
+  //   localStorage.setItem("cityName", JSON.stringify(cityName));
+  // storeL();  
+  // }
+  //   todoForm.addEventListener("submit", function(event) {
+  //     event.preventDefault();
+  //     if (todoText === "") {
+  //       return;
+  //     }
+    
+  //     // Add new todoText to todos array, clear the input
+  //     todos.push(todoText);
+  //     todoInput.value = "";
+    
+  //     // Store updated todos in localStorage, re-render the list
+  //     render();
+  //   });
+    
+    
+  //     var todoText = todoInput.value.trim();
+
+    // console.log(localStorage.getItem("cityName"));
+    
     // response.coord.lat
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
@@ -36,14 +61,13 @@ $(document).ready(function () {
         // Log the queryURL
         var lon = response.coord.lon;
         var lat = response.coord.lat;
-        console.log(lat);
-        // Transfer content to HTML
-        // var currentDate = moment().format('MMMM do YYYY');
-        // console.log(currentDate);
+        let weatherPic = response.weather[0].icon;
+        $(".weather-icon").append(`<img src= http://openweathermap.org/img/wn/${weatherPic}@2x.png>`);
+        console.log(weatherPic);
         $(".city").html("<h1>" + response.name + " Weather Details</h1>");
         $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
         $(".humidity").text("Humidity: " + response.main.humidity + "%");
-console.log(queryURL);
+        console.log(queryURL);
         // Convert the temp to fahrenheit
         var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
@@ -63,62 +87,89 @@ console.log(queryURL);
             "&lon=" +
             lon +
             "&cnt=1",
-        });
-        $("uv-index").text("UV Index: " + response.value);
+        }).then(function (response2) {
+        $(".uv-index").text("UV Index: " + response2[0].value);
+        if (response2[0].value < 3){
+          $(".uv-index").addClass("green");
+          // console.log here
+        } else if (response2[0].value > 10){
+          $(".uv-index").addClass("red");
+      } else {
+        $(".uv-index").addClass("yellow")};
+      
       });
-
+      });
+  
     $.ajax({
       url: forecastURL,
       method: "GET",
     }).then(function (response) {
-      // $(".city-forecast").attr("class", "");
+      $(".city-forecast").attr("class", "");
+      let forecastWeather = $(".forecast-result");
+      console.log(response);
      
-      // for (i = 1; i < 6; i++) {
-        
+      for (i = 1; i < 6; i++) {
+        let forecastData = $("<div class ='col-sm rounded-lg forecast-columns'>");
+        let forecastDay = moment(moment().add('days', i)).format("MM/DD/YYYY");
+        forecastData.append("</h6>" + forecastDay + "</h6>");
+        let forecastPic = response.list[i].weather[0].icon;
+        `<img src= http://openweathermap.org/img/wn/${forecastPic}@2x.png>`
+        forecastData.append("<p> " + `<img src= http://openweathermap.org/img/wn/${forecastPic}@2x.png>` + "</p>");
+        let forecastTemperature = response.list[i].main.temp;
+        forecastData.append("<p>Temperature: " + forecastTemperature + "°</p>")
+        let forecastHumidity = response.list[i].main.humidity;
+        forecastData.append("<p>Humidity: " + forecastHumidity + "%</p>");
+        forecastWeather.append(forecastData);
+        console.log(forecastURL);
+      };
+    });
+  });
+});
+
       //  console.log(forecastData);
         // var forecastDay = moment(moment().add("day", i)).format("MM/DD/YYYY");
         // forecastData.append("<h5" + forecastDay + "<h5>");
-        var forecastData1 = $('.card-city1');
-        var forecastTemperature1 = response.list[1].main.temp; 
-        forecastData1.append("Temperature (F°): " + forecastTemperature1 + ' ');
-        var forecastData2 = $('.card-city2');
-        var forecastTemperature2 = response.list[2].main.temp; 
-        forecastData2.append("Temperature (F°): " + forecastTemperature2 + ' ');
-        var forecastData3 = $('.card-city3');
-        var forecastTemperature3 = response.list[3].main.temp; 
-        forecastData3.append("Temperature (F°): " + forecastTemperature3 + ' ');
-        var forecastData4 = $('.card-city4');
-        var forecastTemperature4 = response.list[4].main.temp; 
-        forecastData4.append("Temperature (F°): " + forecastTemperature4 + ' ');
-        var forecastData5 = $('.card-city5');
-        var forecastTemperature5 = response.list[5].main.temp; 
-        forecastData5.append("Temperature (F°): " +  forecastTemperature5) + ' ';
+        // var forecastData1 = $('.card-city1');
+        // var forecastTemperature1 = response.list[1].main.temp; 
+        // forecastData1.append("Temperature (F°): " + forecastTemperature1 + ' ');
+        // var forecastData2 = $('.card-city2');
+        // var forecastTemperature2 = response.list[2].main.temp; 
+        // forecastData2.append("Temperature (F°): " + forecastTemperature2 + ' ');
+        // var forecastData3 = $('.card-city3');
+        // var forecastTemperature3 = response.list[3].main.temp; 
+        // forecastData3.append("Temperature (F°): " + forecastTemperature3 + ' ');
+        // var forecastData4 = $('.card-city4');
+        // var forecastTemperature4 = response.list[4].main.temp; 
+        // forecastData4.append("Temperature (F°): " + forecastTemperature4 + ' ');
+        // var forecastData5 = $('.card-city5');
+        // var forecastTemperature5 = response.list[5].main.temp; 
+        // forecastData5.append("Temperature (F°): " +  forecastTemperature5) + ' ';
 
-        var forecastHumidity1 = $('.card-city1');
-        var forecastHumid1 = response.list[1].main.humidity;
-        forecastHumidity1.append("Humidity: " + forecastHumid1 + "%")
-        var forecastHumidity2 = $('.card-city2');
-        var forecastHumid2 = response.list[2].main.humidity;
-        forecastHumidity2.append("Humidity: " + forecastHumid2 + "%")
-        var forecastHumidity3 = $('.card-city3');
-        var forecastHumid3 = response.list[3].main.humidity;
-        forecastHumidity3.append("Humidity: " + forecastHumid3 + "%")
-        var forecastHumidity4 = $('.card-city4');
-        var forecastHumid4 = response.list[4].main.humidity;
-        forecastHumidity4.append("Humidity: " + forecastHumid4 + "%")
-        var forecastHumidity5 = $('.card-city5');
-        var forecastHumid5 = response.list[5].main.humidity;
-        forecastHumidity5.append("Humidity: " + forecastHumid5 + "%")
+        // var forecastHumidity1 = $('.card-city1');
+        // var forecastHumid1 = response.list[1].main.humidity;
+        // forecastHumidity1.append("Humidity: " + forecastHumid1 + "%")
+        // var forecastHumidity2 = $('.card-city2');
+        // var forecastHumid2 = response.list[2].main.humidity;
+        // forecastHumidity2.append("Humidity: " + forecastHumid2 + "%")
+        // var forecastHumidity3 = $('.card-city3');
+        // var forecastHumid3 = response.list[3].main.humidity;
+        // forecastHumidity3.append("Humidity: " + forecastHumid3 + "%")
+        // var forecastHumidity4 = $('.card-city4');
+        // var forecastHumid4 = response.list[4].main.humidity;
+        // forecastHumidity4.append("Humidity: " + forecastHumid4 + "%")
+        // var forecastHumidity5 = $('.card-city5');
+        // var forecastHumid5 = response.list[5].main.humidity;
+        // forecastHumidity5.append("Humidity: " + forecastHumid5 + "%")
      
 
-        forecastDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-        console.log(forecastDate);
+        // forecastDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+        // console.log(forecastDate);
         // $("#card1").append(forecastdate)
 
         // forecastData1.append(forecastHumidity);
         // console.log(forecastHumidity);
 
-      });
+        
       // $(".city-forecast").html(
       //   "<h1>" + response.name + " Weather Details</h1>"
       // );
@@ -126,5 +177,4 @@ console.log(queryURL);
       // $(".humidity-forecast").text("Humidity: " + response.main.humidity);
       // $(".container-fluid").empty();
     
-    });
-  });
+
